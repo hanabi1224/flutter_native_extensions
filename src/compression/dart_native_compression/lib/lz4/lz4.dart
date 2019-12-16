1a3f4765ed3730d1cae0aea146dc77ff99f36ee8
hanabi1224 <harlowmoo@gmail.com>
2019-12-16 23:05:28 +0800

    Impl stream api

--- src/compression/dart_native_compression/lib/lz4/lz4.dart
+++ src/compression/dart_native_compression/lib/lz4/lz4.dart
@@ -94,51 +94,16 @@ class Lz4Lib {
   int Function(Pointer<Uint8>, int) _getFrameHeaderSize;
   int Function(Pointer, Pointer<Uint8>, Pointer<Uint64>, Pointer<Uint8>,
       Pointer<Uint64>) _decompressFrame;
-  List<int> _magickHeader = [4, 34, 77, 24];
-  Map<int, int> _blockSizeTable = {
-    4: 1024 * 64,
-    5: 1024 * 256,
-    6: 1024 * 1024,
-    7: 1024 * 1024 * 4,
-  };
+
   Uint8List decompressFrame(Uint8List data) {
     if (!ListEquality().equals(_magickHeader, data.sublist(0, 4)) ||
         data.length < 7) {
       throw Exception('Invalid data');
     }
 
-    // https://github.com/lz4/lz4/blob/v1.9.2/doc/lz4_Frame_format.md
-    final flagByte = data[4];
-    final hasContentSize = flagByte & 0x08 > 0;
-    final hasDictId = flagByte & 0x01 > 0;
-
     final srcBuffer = Uint8ArrayUtils.toPointer(data);
-    final headerSize = _getFrameHeaderSize(srcBuffer, data.length);
-    var estimateDstBufferSize = 0;
-    if (hasContentSize) {
-      var contentSizeBytes = headerSize - 3;
-      if (hasDictId) {
-        contentSizeBytes -= 4;
-      }
-
-      if (contentSizeBytes > 0) {
-        for (var i = 0; i < contentSizeBytes; i++) {
-          estimateDstBufferSize += (data[6 + i] << (8 * i));
-        }
-      }
-    }
-
-    final blockMaxSizeKey = data[5] >> 4 & 0x07;
-    if (_blockSizeTable.containsKey(blockMaxSizeKey)) {
-      final blockMaxSize = _blockSizeTable[blockMaxSizeKey];
-      if (estimateDstBufferSize == 0 || estimateDstBufferSize > blockMaxSize) {
-        estimateDstBufferSize = blockMaxSize;
-      }
-    }
-
-    if (estimateDstBufferSize == 0) {
-      estimateDstBufferSize = _blockSizeTable[7];
-    }
+    final estimateDstBufferSize =
+        _validateFrameAndGetEstimatedDecodeBufferSize(data, srcBuffer);
 
     final contextPtr = allocate<Uint64>(count: 1);
     _createDecompressionContext(contextPtr);
@@ -175,31 +140,92 @@ class Lz4Lib {
     }
   }
 
-  Stream<Uint8List> decompressFrameStreamAsync(
-      Stream<Uint8List> stream) async* {
-    var isFirstChunk = true;
+  Stream<Uint8List> decompressFrameStream(Stream<Uint8List> stream) async* {
+    final contextPtr = allocate<Uint64>(count: 1);
+    _createDecompressionContext(contextPtr);
+    final context = Pointer.fromAddress(contextPtr.elementAt(0).value);
+
+    final dstSizePtr = allocate<Uint64>(count: 1);
+    final srcSizePtr = allocate<Uint64>(count: 1);
+
     var estimateDstBufferSize = 0;
-    await for (final chunk in stream) {
-      if (isFirstChunk) {
-        isFirstChunk = false;
-        estimateDstBufferSize = _getEstimatedDecodeBufferSize(chunk);
+    Pointer<Uint8> srcBuffer;
+    Pointer<Uint8> dstBuffer;
+
+    int nextSrcSize = 0;
+    var sourceBuffer = List<int>();
+    var isFirstChunk = true;
+    try {
+      await for (final chunk in stream) {
+        if (isFirstChunk) {
+          isFirstChunk = false;
+          srcBuffer = Uint8ArrayUtils.toPointer(chunk);
+          estimateDstBufferSize =
+              _validateFrameAndGetEstimatedDecodeBufferSize(chunk, srcBuffer);
+          dstBuffer = allocate<Uint8>(count: estimateDstBufferSize);
+        } else if (nextSrcSize == 0) {
+          return;
+        } else if (nextSrcSize < 0) {
+          throw Exception('Error: $nextSrcSize');
+        }
+
+        sourceBuffer.addAll(chunk);
+        if (sourceBuffer.length >= nextSrcSize) {
+          if (srcBuffer != null) {
+            free(srcBuffer);
+          }
+          srcBuffer = Uint8ArrayUtils.toPointer(sourceBuffer);
+          srcSizePtr.asTypedList(1).setAll(0, [sourceBuffer.length]);
+          dstSizePtr.asTypedList(1).setAll(0, [estimateDstBufferSize]);
+          nextSrcSize = _decompressFrame(
+              context, dstBuffer, dstSizePtr, srcBuffer, srcSizePtr);
+          final consumedSrcSize = srcSizePtr.elementAt(0).value;
+          if (consumedSrcSize >= sourceBuffer.length) {
+            sourceBuffer.clear();
+          } else {
+            sourceBuffer = sourceBuffer.sublist(consumedSrcSize);
+          }
+
+          final dstSize = dstSizePtr.elementAt(0).value;
+          final decompressedChunk =
+              Uint8ArrayUtils.fromPointer(dstBuffer, dstSize);
+          yield Uint8List.fromList(decompressedChunk);
+        }
+      }
+    } finally {
+      _freeDecompressionContext(context);
+      free(contextPtr);
+      free(dstSizePtr);
+      free(srcSizePtr);
+      if (srcBuffer != null) {
+        free(srcBuffer);
+      }
+      if (dstBuffer != null) {
+        free(dstBuffer);
       }
     }
   }
 
-  int _getEstimatedDecodeBufferSize(Uint8List chunk) {
+  List<int> _magickHeader = [4, 34, 77, 24];
+  Map<int, int> _blockSizeTable = {
+    4: 1024 * 64,
+    5: 1024 * 256,
+    6: 1024 * 1024,
+    7: 1024 * 1024 * 4,
+  };
+  int _validateFrameAndGetEstimatedDecodeBufferSize(
+      Uint8List chunk, Pointer<Uint8> chunkPtr) {
     if (!ListEquality().equals(_magickHeader, chunk.sublist(0, 4)) ||
         chunk.length < 7) {
       throw Exception('First chunk too small');
     }
 
     // https://github.com/lz4/lz4/blob/v1.9.2/doc/lz4_Frame_format.md
-    var estimateDstBufferSize;
+    var estimateDstBufferSize = 0;
     final flagByte = chunk[4];
     final hasContentSize = flagByte & 0x08 > 0;
     final hasDictId = flagByte & 0x01 > 0;
-    final srcBuffer = Uint8ArrayUtils.toPointer(chunk);
-    final headerSize = _getFrameHeaderSize(srcBuffer, chunk.length);
+    final headerSize = _getFrameHeaderSize(chunkPtr, chunk.length);
     if (hasContentSize) {
       var contentSizeBytes = headerSize - 3;
       if (hasDictId) {
