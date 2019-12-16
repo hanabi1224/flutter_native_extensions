6a91f19cc8fd90b0e4c0893ccc885532c3b73cde
hanabi1224 <harlowmoo@gmail.com>
2019-12-16 08:18:42 +0800

    Frame compress / decompress native (#4)

--- src/compression/native_compression/src/lz4/mod.rs
+++ src/compression/native_compression/src/lz4/mod.rs
@@ -1,4 +1,6 @@
-use libc::{c_char, c_int};
+use libc::{c_char, c_int, c_uint, c_void, size_t};
+use std::mem::transmute;
+use std::ptr;
 
 mod native;
 use native::*;
@@ -14,39 +16,75 @@ pub extern "C" fn ffi_lz4_version_string() -> *const c_char {
 }
 
 #[no_mangle]
-pub extern "C" fn ffi_lz4_compress_default(
-    src: *const c_char,
-    dst: *mut c_char,
-    src_size: c_int,
-    dst_capacity: c_int,
-) -> c_int {
-    return unsafe { LZ4_compress_default(src, dst, src_size, dst_capacity) };
+pub extern "C" fn ffi_lz4f_get_version() -> c_uint {
+    return unsafe { LZ4F_getVersion() };
 }
 
 #[no_mangle]
-pub extern "C" fn ffi_lz4_decompress_safe(
-    src: *const c_char,
-    dst: *mut c_char,
-    compressed_size: c_int,
-    dst_capacity: c_int,
-) -> c_int {
-    return unsafe { LZ4_decompress_safe(src, dst, compressed_size, dst_capacity) };
+pub extern "C" fn ffi_lz4f_compress_frame_bound(src_size: size_t) -> size_t {
+    return unsafe { LZ4F_compressFrameBound(src_size, ptr::null()) };
 }
 
 #[no_mangle]
-pub extern "C" fn ffi_lz4_compress_bound(input_size: c_int) -> c_int {
-    return unsafe { LZ4_compressBound(input_size) };
+pub extern "C" fn ffi_lz4f_compress_frame(
+    dst_buffer: *mut u8,
+    dst_capacity: size_t,
+    src_buffer: *const u8,
+    src_size: size_t,
+) -> size_t {
+    return unsafe {
+        LZ4F_compressFrame(dst_buffer, dst_capacity, src_buffer, src_size, ptr::null())
+    };
 }
 
 #[no_mangle]
-pub fn ffi_lz4_compress_fast(
-    src: *const c_char,
-    dst: *mut c_char,
-    src_size: c_int,
-    dst_capacity: c_int,
-    acceleration: c_int,
-) -> c_int {
-    return unsafe { LZ4_compress_fast(src, dst, src_size, dst_capacity, acceleration) };
+pub extern "C" fn ffi_lz4f_create_decompression_context(dctx_ptr: *mut size_t) -> size_t {
+    return unsafe { LZ4F_createDecompressionContext(dctx_ptr, ffi_lz4f_get_version()) };
+}
+
+#[no_mangle]
+pub fn ffi_lz4f_free_decompression_context(dctx: *mut c_void) -> size_t {
+    return unsafe { LZ4F_freeDecompressionContext(dctx) };
+}
+
+#[no_mangle]
+pub extern "C" fn ffi_lz4_header_size(src: *const u8, src_size: size_t) -> size_t {
+    return unsafe { LZ4F_headerSize(src, src_size) };
+}
+
+#[no_mangle]
+pub extern "C" fn ffi_lz4f_header_size(src: *const u8, src_size: size_t) -> size_t {
+    return unsafe { LZ4F_headerSize(src, src_size) };
+}
+
+#[no_mangle]
+pub extern "C" fn ffi_lz4f_get_frame_info(
+    dctx: *mut c_void,
+    frame_info_ptr: *mut c_void,
+    src_buffer: *const u8,
+    src_size_ptr: *const size_t,
+) -> size_t {
+    return unsafe { LZ4F_getFrameInfo(dctx, frame_info_ptr, src_buffer, src_size_ptr) };
+}
+
+#[no_mangle]
+pub extern "C" fn ffi_lz4f_decompress(
+    dctx: *mut c_void,
+    dst_buffer: *mut u8,
+    dst_size_ptr: *mut size_t,
+    src_buffer: *const u8,
+    src_size_ptr: *mut size_t,
+) -> size_t {
+    return unsafe {
+        LZ4F_decompress(
+            dctx,
+            dst_buffer,
+            dst_size_ptr,
+            src_buffer,
+            src_size_ptr,
+            ptr::null(),
+        )
+    };
 }
 
 #[test]
@@ -57,4 +95,77 @@ fn test_ffi_lz4_version_number() {
 #[test]
 fn test_ffi_lz4_version_string() {
     ffi_lz4_version_string();
+}
+
+#[test]
+fn test_ffi_lz4f_get_version() {
+    let version = ffi_lz4f_get_version();
+    println!("lz4 frame version: {}", version);
+}
+
+#[test]
+fn test_ffi_lz4f_create_and_free_decompression_context() {
+    let mut address: usize = 0;
+    let p_address: *mut usize = &mut address;
+    ffi_lz4f_create_decompression_context(p_address);
+    println!("decompression context address: {}", address);
+    let decompression_context_ptr = address as *const ();
+    let decompression_context: *mut c_void = unsafe { transmute(decompression_context_ptr) };
+    ffi_lz4f_free_decompression_context(decompression_context);
+}
+
+#[test]
+fn test_ffi_lz4f_compress_frame_and_decompress_frame() {
+    let src: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+    let src_ptr = src.as_ptr();
+    let bound = ffi_lz4f_compress_frame_bound(src.len());
+    println!("lz4 frame bound: {}", bound);
+
+    let mut compressed = Vec::with_capacity(bound);
+    let compressed_ptr = compressed.as_mut_ptr();
+    let compressed_size = ffi_lz4f_compress_frame(compressed_ptr, bound, src_ptr, src.len());
+    unsafe { compressed.set_len(compressed_size) };
+    println!(
+        "lz4 frame compress done, compressed size: {}, array: {:?}",
+        compressed_size, compressed,
+    );
+
+    let compressed_header_size = ffi_lz4f_header_size(compressed_ptr, compressed_size);
+    println!("lz4 frame compress header size: {}", compressed_header_size,);
+
+    let mut decompression_context_address: usize = 0;
+    let decompression_context_address_ptr: *mut usize = &mut decompression_context_address;
+    ffi_lz4f_create_decompression_context(decompression_context_address_ptr);
+    println!(
+        "decompression context address: {}",
+        decompression_context_address
+    );
+    let decompression_context_ptr = decompression_context_address as *const ();
+    let decompression_context: *mut c_void = unsafe { transmute(decompression_context_ptr) };
+
+    let mut dst_size: usize = 100;
+    let dst_size_ptr: *mut usize = &mut dst_size;
+    let mut src_size: usize = compressed_size;
+    let src_size_ptr: *mut usize = &mut src_size;
+
+    let mut decompressed = Vec::with_capacity(dst_size);
+    let decompressed_ptr = decompressed.as_mut_ptr();
+
+    let ret = ffi_lz4f_decompress(
+        decompression_context,
+        decompressed_ptr,
+        dst_size_ptr,
+        compressed_ptr,
+        src_size_ptr,
+    );
+    assert!(ret == 0);
+    unsafe { decompressed.set_len(dst_size) };
+    println!(
+        "ret: {}, src size: {}, dst size: {}, decompressed: {:?}",
+        ret, src_size, dst_size, decompressed
+    );
+
+    assert_eq!(src, decompressed);
+
+    ffi_lz4f_free_decompression_context(decompression_context);
 }
--- src/compression/native_compression/src/lz4/native.rs
+++ src/compression/native_compression/src/lz4/native.rs
@@ -1,4 +1,4 @@
-use libc::{c_char, c_int};
+use libc::{c_char, c_int, c_uint, c_void, size_t};
 
 // Full API doc: https://github.com/lz4/lz4/blob/v1.9.2/lib/lz4.h
 #[allow(non_snake_case)]
@@ -9,35 +9,55 @@ extern "C" {
     // LZ4LIB_API const char* LZ4_versionString (void);   /**< library version string; useful to check dll version */
     pub fn LZ4_versionString() -> *const c_char;
 
-    // https://github.com/lz4/lz4/blob/v1.9.2/lib/lz4.h#L134
-    // LZ4LIB_API int LZ4_compress_default(const char* src, char* dst, int srcSize, int dstCapacity);
-    pub fn LZ4_compress_default(
-        src: *const c_char,
-        dst: *mut c_char,
-        srcSize: c_int,
-        dstCapacity: c_int,
-    ) -> c_int;
+    // LZ4FLIB_API unsigned LZ4F_getVersion(void);
+    pub fn LZ4F_getVersion() -> c_uint;
 
-    // https://github.com/lz4/lz4/blob/v1.9.2/lib/lz4.h#L150
-    // LZ4LIB_API int LZ4_decompress_safe (const char* src, char* dst, int compressedSize, int dstCapacity);
-    pub fn LZ4_decompress_safe(
-        src: *const c_char,
-        dst: *mut c_char,
-        compressedSize: c_int,
-        dstCapacity: c_int,
-    ) -> c_int;
+    // https://github.com/lz4/lz4/blob/v1.9.2/lib/lz4frame.h#L203
+    // LZ4FLIB_API size_t LZ4F_compressFrameBound(size_t srcSize, const LZ4F_preferences_t* preferencesPtr);
+    pub fn LZ4F_compressFrameBound(src_size: size_t, preferences_ptr: *const c_void) -> size_t;
 
-    // https://github.com/lz4/lz4/blob/v1.9.2/lib/lz4.h#L173
-    // LZ4LIB_API int LZ4_compressBound(int inputSize);
-    pub fn LZ4_compressBound(inputSize: c_int) -> c_int;
+    // https://github.com/lz4/lz4/blob/v1.9.2/lib/lz4frame.h#L211
+    // LZ4FLIB_API size_t LZ4F_compressFrame(void* dstBuffer, size_t dstCapacity,
+    //     const void* srcBuffer, size_t srcSize,
+    //     const LZ4F_preferences_t* preferencesPtr);
+    pub fn LZ4F_compressFrame(
+        dst_buffer: *mut u8,
+        dst_capacity: size_t,
+        src_buffer: *const u8,
+        src_size: size_t,
+        preferences_ptr: *const c_void,
+    ) -> size_t;
 
-    // https://github.com/lz4/lz4/blob/v1.9.2/lib/lz4.h#L184
-    // LZ4LIB_API int LZ4_compress_fast (const char* src, char* dst, int srcSize, int dstCapacity, int acceleration);
-    pub fn LZ4_compress_fast(
-        src: *const c_char,
-        dst: *mut c_char,
-        srcSize: c_int,
-        dstCapacity: c_int,
-        acceleration: c_int,
-    ) -> c_int;
+    // LZ4FLIB_API LZ4F_errorCode_t LZ4F_createDecompressionContext(LZ4F_dctx** dctxPtr, unsigned version);
+    pub fn LZ4F_createDecompressionContext(dctx_ptr: *mut size_t, version: c_uint) -> size_t;
+
+    // LZ4FLIB_API LZ4F_errorCode_t LZ4F_freeDecompressionContext(LZ4F_dctx* dctx);
+    pub fn LZ4F_freeDecompressionContext(dctx: *mut c_void) -> size_t;
+
+    // https://github.com/lz4/lz4/blob/v1.9.2/lib/lz4frame.h#L370
+    // size_t LZ4F_headerSize(const void* src, size_t srcSize);
+    pub fn LZ4F_headerSize(src: *const u8, src_size: size_t) -> size_t;
+
+    // LZ4FLIB_API size_t LZ4F_getFrameInfo(LZ4F_dctx* dctx,
+    //     LZ4F_frameInfo_t* frameInfoPtr,
+    //     const void* srcBuffer, size_t* srcSizePtr);
+    pub fn LZ4F_getFrameInfo(
+        dctx: *mut c_void,
+        frame_info_ptr: *mut c_void,
+        src_buffer: *const u8,
+        src_size_ptr: *const size_t,
+    ) -> size_t;
+
+    // LZ4FLIB_API size_t LZ4F_decompress(LZ4F_dctx* dctx,
+    //     void* dstBuffer, size_t* dstSizePtr,
+    //     const void* srcBuffer, size_t* srcSizePtr,
+    //     const LZ4F_decompressOptions_t* dOptPtr);
+    pub fn LZ4F_decompress(
+        dctx: *mut c_void,
+        dst_buffer: *mut u8,
+        dst_size_ptr: *mut size_t,
+        src_buffer: *const u8,
+        src_size_ptr: *mut size_t,
+        d_opt_ptr: *const c_void,
+    ) -> size_t;
 }
